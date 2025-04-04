3b8a03f6d08b8d636333c5166b8601fa891fab2b
Daniel Moraes <danielbibit@gmail.com>
2025-04-04 00:09:53 +0000

    Remove docker compose files from containers role

--- roles/containers/files/docker_upgrade.sh
+++ roles/containers/files/docker_upgrade.sh
@@ -2,39 +2,50 @@
 echo 'Pruning old images...'
 docker image prune -af
 
-echo
-echo 'Saving current images hash id...'
 # Save current image hash for rollback
+echo -e '\nSaving current images hash id...'
 echo >> /home/nas/scripts/docker_pre_upgrade_digest.log
 date >> /home/nas/scripts/docker_pre_upgrade_digest.log
 docker image list --digests | awk '{print $1 "   " $3}' >> /home/nas/scripts/docker_pre_upgrade_digest.log
 
+# Compute project list using docker compose ls (without jq)
+PROJECTS=$(docker compose ls | tail -n +2)
 
-echo
-echo 'Pulling new images...'
-echo
-# Pull new images
-for file in $(ls -1 /home/nas/compose | grep yml);
-do
-    docker-compose -f /home/nas/compose/${file} pull;
-done
+# Pull new images using docker compose v2 and specifying file path
+echo -e '\nPulling new images...\n'
+while read -r project status compose_path; do
+    echo "Pulling images for project $project from $compose_path"
+    docker compose -p $project -f $compose_path pull
+done <<< "$PROJECTS"
 
-
-echo
-echo 'Creating zfs snapshot...'
 # Create zfs snapshot
+echo -e '\nCreating zfs snapshot...'
 /usr/sbin/zfs snapshot Users/docker@$(date +%Y_%m_%d)
 
+# Upgrade docker containers using docker compose v2
+echo -e '\nUpgrading docker containers...\n'
+while read -r project status compose_path; do
+    echo "Upgrading containers for project $project from $compose_path"
+    docker compose -p $project -f $compose_path up -d
+done <<< "$PROJECTS"
 
-echo
-echo 'Upgrading docker containers...'
-echo
-# Upgrade images
-for file in $(ls -1 /home/nas/compose | grep yml);
-do
-    docker-compose --project-name ${file} -f /home/nas/compose/${file} up -d;
-done
+# Delete old snapshots
+FS="Users/docker"
 
+# List snapshots sorted by creation date (oldest first)
+SNAPSHOTS=($(zfs list -t snapshot -o name -s creation | grep "^$FS@"))
 
-#TODO delete old snapshots
-# https://serverfault.com/questions/340837/how-to-delete-all-but-last-n-zfs-snapshots
+# Count snapshots
+NUM_SNAPSHOTS=${#SNAPSHOTS[@]}
+
+# Keep only the 4 most recent ones
+if [ "$NUM_SNAPSHOTS" -gt 4 ]; then
+    TO_DELETE=$((NUM_SNAPSHOTS - 4))
+    echo "Deleting $TO_DELETE old snapshots..."
+    for ((i=0; i<$TO_DELETE; i++)); do
+        echo "Destroying ${SNAPSHOTS[$i]}"
+        zfs destroy "${SNAPSHOTS[$i]}"
+    done
+else
+    echo "No snapshots to delete. Keeping all $NUM_SNAPSHOTS snapshots."
+fi
