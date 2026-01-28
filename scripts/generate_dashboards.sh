ff6335ca5944aa117fea15857b68b74d7c8793ba
Daniel Moraes <danielbibit@gmail.com>
2026-01-28 20:58:02 -0300

    Fix grafana dashboard generation

--- scripts/generate_dashboards.sh
+++ scripts/generate_dashboards.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
 
-for script in /workspaces/nas-ansible/dashboards_python/*.py; do
+for script in $(pwd)/dashboards_python/*.py; do
     filename=$(basename "$script" .py)
-    output_file="/workspaces/nas-ansible/roles/grafana/files/dashboards/${filename}.json"
+    output_file="$(pwd)/roles/grafana/files/dashboards/${filename}.json"
     python "$script" > "$output_file"
 done
